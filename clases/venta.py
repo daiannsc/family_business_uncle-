5f53d8420bcfc3e669da3348805e938d6bbdadad
daiannsc <ericdaiann@hotmail.com>
2024-11-18 20:10:20 +0100

    family_business_uncle-

---
+++ clases/venta.py
@@ -0,0 +1,22 @@
+
+from datetime import datetime
+
+
+class Venta:
+    def __init__(self, cliente, lista_de_productos):
+        self.cliente = cliente 
+        self.lista_de_productos = lista_de_productos
+        self.fecha = datetime.now()
+        self.total = self.calcular_total()
+
+    def calcular_total(self):
+        return sum(Producto.precio for Producto in self.lista_de_productos)
+    
+    def registrar_venta(self):
+        self.cliente.registrar_compra(self)
+        return f'Venta Registrada {self.mostrar_informacion()}'
+
+    def mostrar_informacion(self):
+        productos =  ','.join([Producto.nombre for Producto in self.lista_de_productos])
+        return f'Cliente: {self.cliente.nombre}, Prodcuto: {productos}, Total: {self.total}'
+    
